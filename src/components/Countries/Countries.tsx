8fda91b873db2bf069685f59bcb031bfcdad98d4
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-09-06 16:51:14 +0600

    country flags view

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type { CountryType } from "../../type";
 import Country from "../country/Country";
 import "./Countries.css";
@@ -8,14 +8,60 @@ export interface CountriesProps {
 }
 
 export default function Countries({ countriesPromise }: CountriesProps) {
+  const [visitedCountries, setVisitedCountries] = useState<CountryType[]>([]);
+
+  const [visitedFlags, setVisitedFlags] = useState<string[]>([]);
   const countries = use(countriesPromise);
-  console.log(countries);
+
+  const handleVisitedCountry = (country: CountryType): void => {
+    // const newVisitedCountries = [...visitedCountries, country];
+    // setVisitedCountries(newVisitedCountries);
+   const exists = visitedCountries.find(c=>c.ccn3.ccn3===country.ccn3.ccn3)
+    if(exists){
+      const remainingcountries = visitedCountries.filter(c=>c.ccn3.ccn3 !== country.ccn3.ccn3);
+      setVisitedCountries(remainingcountries);
+    }
+    else {
+      const newVisitedCountries = [...visitedCountries, country];
+      setVisitedCountries(newVisitedCountries);
+    }
+  };
+
+  const handleVisitedFlag = (flag: string): void => {
+    console.log("flag visited", flag);
+    if (visitedFlags.includes(flag)) {
+      const remainingFlags = visitedFlags.filter((f) => f !== flag);
+      setVisitedFlags(remainingFlags)
+    } else {
+      const newVisitedFlags = [...visitedFlags, flag];
+      setVisitedFlags(newVisitedFlags);
+    }
+  };
+  // console.log(countries);
   return (
     <div>
-      <h2>Countries:</h2>
+      <h2>Countries:{countries.length}</h2>
+      <h4>Visited Countries:{visitedCountries.length}</h4>
+      <h4>Visited Flags:{visitedFlags.length}</h4>
+      <div>
+        <ul>
+          
+          {visitedCountries.map(country => <li>{country.name.common}</li>)}
+        </ul>
+        
+      </div>
+      <div className="visited-flags">
+        {visitedFlags.map((flag) => <img key={flag} src={flag} />)}
+      </div>
+      
       <div className="countries">
         {countries.map((country) => (
-          <Country key={country.ccn3.ccn3} country={country} />
+          <Country
+            key={country.ccn3.ccn3}
+            country={country}
+            handleVisitedCountry={handleVisitedCountry}
+            handleVisitedFlag={handleVisitedFlag}
+          />
         ))}
       </div>
     </div>
--- src/components/country/Country.tsx
+++ src/components/country/Country.tsx
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import type { CountryType } from "../../type";
 import "./Country.css";
 
 export interface CountryProps {
   country: CountryType;
+  handleVisitedCountry : (country:CountryType)=>void
+  handleVisitedFlag : (flag:string)=>void
 }
 
-export default function Country({ country }: CountryProps) {
+export default function Country({ country, handleVisitedCountry,handleVisitedFlag }: CountryProps) {
+  const [visited, setVisited] = useState<boolean>(false);
+
+  const handleVisited = () => {
+    //   if(visited===true){
+    //       setVisited(false);
+    //   }else{setVisited(true);}
+    setVisited(!visited);
+    handleVisitedCountry(country);
+  };
+  
+  
+
   return (
-    <div className="country">
+    <div className={`country ${visited ? 'country-visted':''}`}>
       <h3>{country.name.official}</h3>
-      <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h4>Capital: {country.capital.capital}</h4>
       <h4>Population: {country.population.population}</h4>
-      <button>Visited</button>
+      <button onClick={handleVisited}>{visited? 'Visited':'Mark as visited'}</button>
+      <button onClick={()=>handleVisitedFlag(country.flags.flags.png)}>Add flag as visited</button>
     </div>
   );
 }
